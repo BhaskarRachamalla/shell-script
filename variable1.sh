643e1013d00a4a89ed892264d616f1ace605068c
BhaskarRachamalla <bhaskar.apr30@gmail.com>
2024-04-19 18:40:08 +0530

    sample code 1

--- variable1.sh
+++ variable1.sh
@@ -5,7 +5,7 @@ PERSON2=20
 
 
 sum="$(($PERSON1+$PERSON2))"
-subtract="$(($PERSON1+$PERSON2))"
+subtract="$(($PERSON1-$PERSON2))"
 echo "the sum of two numbers is:$sum"
 echo "the subtraction of two numbers is:$subtract"
 
