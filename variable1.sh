d64ca7203d2ff9751f5f784f2093de62b5c8f96e
BhaskarRachamalla <bhaskar.apr30@gmail.com>
2024-04-19 19:08:33 +0530

    sample code 1

--- variable1.sh
+++ variable1.sh
@@ -3,10 +3,10 @@
 read $1
 read $2
 
-sum="$(($1+$2))"
-subtract="$(($1-$2))"
-echo "the sum of two numbers is:$sum"
-echo "the subtraction of two numbers is:$subtract"
+$sum="$(($1+$2))"
+$subtract="$(($1-$2))"
+#echo "the sum of two numbers is:$sum"
+#echo "the subtraction of two numbers is:$subtract"
 
 read  USER
 echo "user name is $USER"
