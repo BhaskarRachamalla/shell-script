ed26e101f03e7efbe18fc8bdea336f189ab7e52b
BhaskarRachamalla <bhaskar.apr30@gmail.com>
2024-04-19 18:52:35 +0530

    sample code 1

--- variable1.sh
+++ variable1.sh
@@ -7,10 +7,12 @@ subtract="$(($1-$2))"
 echo "the sum of two numbers is:$sum"
 echo "the subtraction of two numbers is:$subtract"
 
-if ($sum -gte 100){
+if ($sum -gte 100)
+{
 echo "first class"
 }
-else{ 
+else
+{ 
 echo "fail"
 }
 
