9a4480591ed4db50747a4f5fce4a6a0f97666f67
BhaskarRachamalla <bhaskar.apr30@gmail.com>
2024-04-19 23:20:20 +0530

    sample code 1

--- whilecondition.sh
+++ whilecondition.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 i=1
-j=6
+j=2
 
 k=$(($i+$j))
 # the number 4 can be the limit to
 
 # iterate the loop
-echo "the sum of :$k"
+echo "the sum of $i and $j is:$k"
 
 while [ $k -le 4 ];
 do
