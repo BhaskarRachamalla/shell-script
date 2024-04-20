37c43e6da20f783fc4daadd6b8da60fc714d83f4
BhaskarRachamalla <bhaskar.apr30@gmail.com>
2024-04-20 17:50:56 +0530

    installations

--- functions.sh
+++ functions.sh
@@ -8,6 +8,11 @@ USERid=$(id -u)
 X(){
        echo "exit status: $1"
        echo "what are you doing: $2"
+if [ $? -ne 0 ]
+then
+   echo "installing git failure"
+   else
+   echo "installing git success"
 }
 
 if [ $USERid -ne 0 ]
@@ -19,6 +24,7 @@ else
 fi 
 
 dnf install mysql -y 
+dnf install git -y 
 
 X $? "installating mysql"
 
